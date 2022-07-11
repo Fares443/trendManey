95e054ef74739e9db27d29f847e3c22c80790fe1
Fares443 <Fm#55555>
2022-07-11 15:55:11 +0200

    lad

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,12 @@
 // ignore_for_file: unused_import, depend_on_referenced_packages, avoid_print
 import 'package:trend_money1/BottomNaviga/bottomNavigationBar.dart';
 import 'package:trend_money1/Responsive/info_widget.dart';
-import 'package:trend_money1/screen/many.dart';
-import 'package:trend_money1/soshel/facebook.dart';
-import 'package:trend_money1/soshel/intstagram.dart';
-import 'package:trend_money1/soshel/tiktok.dart';
-import 'package:trend_money1/BottomNaviga/vip.dart';
-import 'package:trend_money1/soshel/youtube.dart';
-import 'package:trend_money1/test.dart';
 import 'package:device_preview/device_preview.dart';
-import 'package:trend_money1/view/screen/auth/login.dart';
-import 'package:trend_money1/view/screen/auth/signup.dart';
 import '/core/constant/color.dart';
 import '/core/localization/translation.dart';
 import '/core/services/services.dart';
 import '/routes.dart';
 import '/view/screen/language.dart';
-import '/view/screen/onboarding.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'core/localization/changelocal.dart';
@@ -61,9 +51,9 @@ class MyApp extends StatelessWidget {
                 fontWeight: FontWeight.bold,
                 fontSize: 14),
             bodyText2:
-                TextStyle(height: 2, color: AppColor.grey, fontSize: 14)),
-        primarySwatch: Colors.blue,
-      ),
+            TextStyle(height: 2, color: AppColor.grey, fontSize: 14)),
+            primarySwatch: Colors.blue,
+             ),
      
        home: InfoWidget(
             builder: (context, deviceInfo) {
